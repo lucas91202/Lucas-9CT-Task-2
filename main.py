a93c0d45ba8eb4895ac638a880f13b777e294828
lucas91202 <lucas.luo@education.nsw.gov.au>
2026-08-13 19:03:19 +1000

    worked on my functions before I recieve the sensors

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-sound = Pin(18,Pin.IN, Pin.PULL_UP)
-led = Pin(16, Pin.OUT)
+from machine import Pin, ADC
+from time import sleep
+sound = Pin(20,Pin.IN, Pin.PULL_UP)
+r = Pin(16,Pin.OUT)
+g = Pin(17,Pin.OUT)
+b = Pin(18,Pin.OUT)
 
 def high():
-
+    r.value(1)
+    while r.value == 1:
+        
 def low():
 
 def optimal():
@@ -18,6 +24,7 @@ def sound():
             sleep(0.1)
         else:
             led.value(0)
+
 def timer():
 
 
@@ -25,10 +32,34 @@ def main():
     #temp = sensor code
     #wet = sensor code
     while True:
-        if '''temp >= 21''':
+        if temp >= 21:
+            if temp > 22:
+                high()
+            else:
+                warning()
+        else:
+            if temp <= 16:
+                if temp < 15:
+                    low()
+                else:
+                    warning()
+            else:
+                optimal()
 
-from machine import Pin, ADC
-from time import sleep
+        if wet >= 51:
+            if temp > 60:
+                high()
+            else:
+                warning()
+        else:
+            if wet <= 34:
+                if wet < 30:
+                    low()
+                else:
+                    warning()
+            else:
+                optimal()
+
 
 
 
