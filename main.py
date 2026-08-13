49c92cd2dde69a8a56c707b88132dbba6e1e6d27
lucas91202 <lucas.luo@education.nsw.gov.au>
2026-08-13 19:05:09 +1000

    added more to my functions

--- main.py
+++ main.py
@@ -1,32 +1,67 @@
-from machine import Pin, ADC
-from time import sleep
+from machine import Pin, Timer
+from time import sleep, time
 sound = Pin(20,Pin.IN, Pin.PULL_UP)
+buzzer = Pin(0,Pin.OUT)
 r = Pin(16,Pin.OUT)
 g = Pin(17,Pin.OUT)
 b = Pin(18,Pin.OUT)
 
+def timer():
+    global start
+    start = time.ticks_s()
+
+def checktimer():
+    finish = time.tick_s() - start
+    return finish
+
 def high():
     r.value(1)
-    while r.value == 1:
-        
+    g.value(0)
+    b.value(0)
+    while r.value() == 1:
+        timer()
+        if sound() == True:
+            if timer() > 60:
+                buzzer.value(1)
+            else:
+                buzzer.value(0)
+        else:
+            buzzer.value(0)
+                
 def low():
+    r.value(0)
+    g.value(0)
+    b.value(1)
+    while b.value() == 1:
+        timer()
+        if sound() == True:
+            if timer() > 60:
+                buzzer.value(1)
+            else:
+                buzzer.value(0)
+        else:
+            buzzer.value(0)
 
 def optimal():
+    r.value(0)
+    g.value(1)
+    b.value(0)
+    buzzer.value(0)
 
 def warning():
+    r.value(1)
+    g.value(1)
+    b.value(0)
 
 def sound():
-    led.value(0)
-    while True:
-        sound = sound.value()
-        if sound == 1:
-            led.value(1)
-            sleep(0.1)
-        else:
-            led.value(0)
-
-def timer():
-
+    if buzzer.value() or r.value() == 1:
+        while True:
+            sound = sound.value()
+            if sound == 1:
+                buzzer.value(0)
+                sleep(0.1)
+            else:
+                led.value(0)
 
 def main():
     #temp = sensor code
